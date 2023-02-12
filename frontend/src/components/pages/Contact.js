88105d161d1d809862dff40450b4ded9d660569d
Kuldeep <bundela@opendress.com>
2023-02-12 23:18:44 +0100

    made the homepage responsive and created a contact form

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -7,9 +7,24 @@ export default function Contact() {
   return(
 
     <div className="cont-div">
+      <h1 className='contact'>Kontakt</h1>     
+      <div className='contact-form'>
+        <form action='/action_page.js'>
+         <label for="fname">Křestní jméno</label>
+         <input type="text" id="fname" name="firstname" placeholder="Křestní jméno"/>
 
-        <h1 className='contact'>Kontakt</h1>     
+         <label for="lname">Příjmení</label>
+         <input type="text" id="lname" name="lastname" placeholder="Příjmení"/>
 
+         <label for="email">E-mail</label>
+         <input type="text" id="email" name="lastname" placeholder="E-mail"/>
+
+          <label for="message">Zpráva</label>
+          <textarea id="message" name="lastname" placeholder="Zde popište svůj požadavek"></textarea>
+
+          <button type="submit" value="Submit">Předložit</button>
+        </form>
+      </div>
     </div>
   ) 
 }
